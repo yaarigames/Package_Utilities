6b5baaa103a8adb664b286777a1fc18f6a15ee42
Abhi86shek <sonkar.abhishek86@gmail.com>
2022-05-09 17:29:08 +0530

    Added a method in IServiceLocator

--- Runtime/TagSystem/ServiceLocator/IServiceLocator.cs
+++ Runtime/TagSystem/ServiceLocator/IServiceLocator.cs
@@ -5,9 +5,12 @@ namespace SAS.TagSystem
 {
     public interface IServiceLocator : IBindable
     {
+        void Add<T>(object service, string tag = "");
+        void Add(Type type, object service, string tag = "");
         T Get<T>(string tag = "");
         bool TryGet<T>(out T service, string tag = "");
         bool TryGet(Type type, out object service, string tag = "");
         IEnumerable<T> GetAll<T>(string tag = "");
+        T GetOrCreate<T>(string tag = "");
     }
 }
