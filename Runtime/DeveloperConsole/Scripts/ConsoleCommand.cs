09e953dacba862054c81ecef7f8ead144e27e451
Abhi86Shek <sonkar.abhishek86@gmail.com>
2023-12-24 18:38:23 +0530

    added developer console

---
+++ Runtime/DeveloperConsole/Scripts/ConsoleCommand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SAS.Utilities.DeveloperConsole
+{
+    public abstract class ConsoleCommand : ScriptableObject, IConsoleCommand
+    {
+        [SerializeField] private string commandWord = string.Empty;
+
+        public string CommandWord => commandWord;
+
+        public abstract bool Process(string[] args);
+    }
+}
---
+++ Runtime/DeveloperConsole/Scripts/IConsoleCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace SAS.Utilities.DeveloperConsole
+{
+    public interface IConsoleCommand
+    {
+        string CommandWord { get; }
+        bool Process(string[] args);
+    }
+}
