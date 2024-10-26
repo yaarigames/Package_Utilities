47ff0a15f1c6a095bf66ffbd13b62f68f062b70b
Abhi86Shek <sonkar.abhishek86@gmail.com>
2024-10-26 15:18:42 +0530

    blackboard

---
+++ Runtime/Blackboard/StringExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class StringExtensions
+{
+    public static int ComputeFNV1aHash(this string str)
+    {
+        uint hash = 2166136261;
+        foreach (char c in str)
+        {
+            hash = (hash ^ c) * 16777619;
+        }
+        return unchecked((int)hash);
+    }
+}
